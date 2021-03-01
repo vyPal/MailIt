fde0062a1d9e200cd9afe76a33b834b218f243b3
Jakub Palacký <kubik.palacky@gmail.com>
2021-03-01 15:46:42 +0000

    Added base for slash commands

---
+++ index.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const fs = require("fs");
+const settings = require('discord-server-settings')
+
+client.ws.on('INTERACTION_CREATE', async interaction => {
+  const command = interaction.data.name.toLowerCase();
+  const args = interaction.data.options;
+  console.log(command)
+  client.api.interactions(interaction.id, interaction.token).callback.post({data: {
+    type: 4,
+    data: {
+      content: "It works!"
+    }
+  }})
+})
+
+client.login(process.env.DISCORD_TOKEN);
