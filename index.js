2c5935a843dc8e071cfb99af20e547946f5c7f72
vyPal <66716025+vyPal@users.noreply.github.com>
2021-03-02 10:07:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const settings = require('discord-server-settings')
 client.ws.on('INTERACTION_CREATE', async interaction => {
   const command = interaction.data.name.toLowerCase();
   const args = interaction.data.options;
-  console.log(command)
+  console.log(command+"\n"+args[0].options[0].options[0].value)
   client.api.interactions(interaction.id, interaction.token).callback.post({data: {
     type: 4,
     data: {
